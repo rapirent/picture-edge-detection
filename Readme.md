401800bcc7956ce4da9e1807ef8f2b7fd6aab894
rapirent <rapirent@gmail.com>
2018-05-23 20:54:28 +0800

    Add readme and python sciprt

---
+++ Readme.md
@@ -0,0 +1,36 @@
+# Multimedia system and application Homework 2
+- 多媒體系統與應用 HW2
+
+## requirements
+- python3.6
+- opencv
+
+## usage
+
+- install required package
+
+```sh
+$ pip install -r requirements.txt
+```
+
+- run the script
+    - it will load the `001.jpg`, `002.jpg` in this directory
+
+```sh
+$ python3 hw2.py
+```
+
+- or you can run the jupyter notebook
+    - you should have jupyter or conda installed
+
+```sh
+$ jupyter-notebook HW2.ipynb
+```
+
+## Author
+
+丁國騰
+
+## LICENSE
+
+MIT
---
+++ hw2.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+import math
+
+
+# 讀入檔案
+
+img = cv2.imread('./001.jpg',  cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('./002.jpg', cv2.IMREAD_GRAYSCALE)
+img.shape
+img2.shape
+
+
+# 進行直方圖平均化 (左為原圖，右為平均化結果，使用numpy hstack合併）
+
+equ = cv2.equalizeHist(img)
+res = np.hstack((img,equ))
+cv2.imwrite('001-equalizeHist.png', equ)
+
+
+# 執行X和Y方向的Sobel算子
+
+x = cv2.Sobel(img,cv2.CV_16S,1,0)
+y = cv2.Sobel(img,cv2.CV_16S,0,1)
+absX = cv2.convertScaleAbs(x)
+absY = cv2.convertScaleAbs(y)
+dst = cv2.addWeighted(absX,0.5,absY,0.5,0)
+cv2.imwrite('001-sobelX.png', absX)
+cv2.imwrite('001-sobelY.png', absY)
+cv2.imwrite('001-sobel.png', dst)
+
+
+# 第二張圖也一樣
+
+equ = cv2.equalizeHist(img2)
+cv2.imwrite('002-equalizeHist.png', equ)
+x = cv2.Sobel(img2,cv2.CV_16S,1,0)
+y = cv2.Sobel(img2,cv2.CV_16S,0,1)
+absX = cv2.convertScaleAbs(x)
+absY = cv2.convertScaleAbs(y)
+dst = cv2.addWeighted(absX,0.5,absY,0.5,0)
+cv2.imwrite('002-sobelX.png', absX)
+cv2.imwrite('002-sobelY.png', absY)
+cv2.imwrite('002-sobel.png', dst)
